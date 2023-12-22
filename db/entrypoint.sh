2a4eeef3a36ffd8570828ab6d2926b47375d1b95
MohaamedHamdy <m7mdhamdy45@gmail.com>
2023-12-22 21:36:49 +0200

    up

--- db/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Change permissions during container startup
-chown -R postgres:postgres /var/lib/postgresql/data
-chown -R postgres:postgres /var/run/postgresql
-
-# Call the default entrypoint script of the parent image
-/docker-entrypoint.sh "$@"
